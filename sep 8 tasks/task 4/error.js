aa42fd763d53e7e06af30139408c1a7b4cf7430e
divya sri <divya.manusani9@gmail.com>
2020-09-09 16:23:29 +0530

    added new files

---
+++ sep 8 tasks/task 4/error.js
@@ -0,0 +1,15 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://dogcat.ceo/api/breeds/list/all");
+ 
+xhr.onreadystatechange = function () {
+    console.log("readyState = " + this.readyState + ", status = " + this.status);
+    if (this.readyState == 4 && this.status == 200) {
+        var result = this.responseText;
+        console.log(JSON.parse(result));
+    }
+    else{
+        console.log("Error==", xhr.statusText);
+    }
+};
+ 
+xhr.send();
