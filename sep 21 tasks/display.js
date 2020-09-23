e7fde391e7d5d8f2e7e510c3bad6c3f4c669bc86
divya sri <divya.manusani9@gmail.com>
2020-09-23 23:55:24 +0530

    8 digit Random Number, Date, getting form details in table

--- sep 21 tasks/display.js
+++ sep 21 tasks/display.js
@@ -1,3 +1,51 @@
+var container=document.createElement('container');
+var row1=document.createElement('row');
+var col1=document.createElement('col-md-10');
+var input1=document.createElement('input');
+var input2=document.createElement('input');
+var br1=document.createElement('br');
+var br2=document.createElement('br');
+
+input1.setAttribute('type','text');
+input1.setAttribute('name','Random');
+input1.setAttribute('id','random');
+input1.setAttribute('class','input-lg');
+input2.setAttribute('type','button');
+input2.setAttribute('class','btn btn-success input-lg');
+input2.setAttribute('value','Generate Random Number');
+input2.setAttribute('onclick','generate()');
+
+col1.append(input1,br1,br2,input2);
+row1.append(col1);
+container.append(row1);
+
+var row1=document.createElement('row');
+var col1=document.createElement('col-md-8');
+var input1=document.createElement('input');
+var input2=document.createElement('input');
+var br1=document.createElement('br');
+var br2=document.createElement('br');
+var br3=document.createElement('br');
+var br4=document.createElement('br');
+var br=document.createElement('br');
+var brz=document.createElement('br');
+var para=document.createElement('p');
+para.setAttribute('id','demo');
+
+input1.setAttribute('type','date');
+input1.setAttribute('name','dob');
+input1.setAttribute('id','dateofbirth');
+input1.setAttribute('class','input-lg');
+input2.setAttribute('type','button');
+input2.setAttribute('class','btn btn-info input-lg');
+input2.setAttribute('value','Get Details');
+input2.setAttribute('onclick','getdetails()');
+
+col1.append(br,brz,input1,br1,br2,input2,br3,br4,para);
+row1.append(col1);
+container.append(row1);
+
+document.body.append(container);
 
 var numArr=[];
 var num;
@@ -17,12 +65,12 @@ function getdetails(){
     var dateOfBirth = Date.parse(dob);//in millisec from January 1, 1970
     var todayDate = Date.now(); // milliseconds
     var millisec=todayDate-dateOfBirth;
-    var sec=(millisec/1000).toFixed(2);
-    var min=(sec/60).toFixed(2);
-    var hrs=(min/60).toFixed(2);
-    var days=(hrs/24).toFixed(2);
-    var months=(days/30).toFixed(2);
-    var year=(months/12).toFixed(2);
+    var sec=(millisec/1000).toFixed(0);
+    var min=(sec/60).toFixed(0);
+    var hrs=(min/60).toFixed(0);
+    var days=(hrs/24).toFixed(0);
+    var months=(days/30).toFixed(0);
+    var year=(months/12).toFixed(0);
 
     var str=`I am ${year} years ,<br>${months} months ,<br>${days} days, <br>
     ${hrs} hours, <br>${min} minutes, <br>${sec} seconds,<br> ${millisec} milliseconds old`;
